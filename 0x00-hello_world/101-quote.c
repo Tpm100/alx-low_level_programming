d0ef66c42234398d29db7ab027ce39c14a767e66
Tpm100 <tokunbo2420@gmail.com>
2022-09-12 09:23:09 -0700

    UNIX is basically an operating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,15 @@
 #include <unistd.h>
 
 /**
- * main - Prints a code in C
+ * main - print the string in the put function
  *
- * Return: Always 1 (success)
- * /
- int main(void)
- {
-      write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
-      return (1);
- }
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 1
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
 
