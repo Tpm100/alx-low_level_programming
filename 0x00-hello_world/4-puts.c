1316952b6e0e6fc1a0cfd4f9ae67adac77a66553
Tpm100 <tokunbo2420@gmail.com>
2022-09-11 18:01:55 -0700

    coding

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+ /**
+ * main
+ * maim - print the string in the put function
+ *
+ * Description: using the main function
+ * Return: 0
+ */
+ Int main(void)
+ {
+         put(*\ "programming is like building a multilingual puzzle");
+         return (0);
