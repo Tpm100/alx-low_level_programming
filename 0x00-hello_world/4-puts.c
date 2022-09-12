566307c1d00c598c37fc138bd83d5120f7cbc31f
Tpm100 <tokunbo2420@gmail.com>
2022-09-12 06:46:19 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - print the string in the put function
+ * this program prints "programming is like buiding a puzzle"
+ * Return: 0
+ */
+int main(void)
+{
+        puts("\"programing is like buoilding a puzzle");
+        return (0);
+}	
