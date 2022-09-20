d85130a8de1d867a79c9c9c95e4ef15ace5f7895
Tpm100 <tokunbo2420@gmail.com>
2022-09-20 06:15:51 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strlem - returns the length of a string
+ * _strlen - returns the length of a string
  * @s: string
  * Return: returns length as integer;
  */
@@ -10,7 +10,7 @@ int _strlen(char *s)
 {
 	int len = 0;
 
-	while (*(s = len) != '\0')
+	while ( s[len] != '\0')
 		len++;
 
 	return (len);
