fbf83b1e2bd377323b527e5daa0a035b0b66fbb7
Tpm100 <tokunbo2420@gmail.com>
2022-09-22 08:08:14 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,12 +8,13 @@
 void print_times_table(int n)
 {
 	int a, b, op;
+
 	if (n >= 0 && n <= 15)
 	{
 		for (a = 0; a <= n; a++)
 		{
 			_putchar(48);
-			for (b = 1; b <=n; b++)
+			for (b = 1; b <= n; b++)
 			{
 				op = a * b;
 				_putchar(44);
