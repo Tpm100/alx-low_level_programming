0ae176497cd7280285401e24317c772387aac1e1
Tpm100 <tokunbo2420@gmail.com>
2022-09-22 08:21:19 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,7 +11,7 @@ int main(void)
 	sum3 = 0;
 	sum5 = 0;
 	sum = 0;
-	for (i = 0; i < 1024, ++i)
+	for (i = 0; i < 1024, ++i);
 	{
 		if ((i % 3) == 0)
 		{
@@ -20,10 +20,9 @@ int main(void)
 		{			
 			sum5 = sum5 + i;
 		}
-		{
+	}
 		sum = sum3 + sum5;
 		printf("%lu\n", sum);
 	        return (0);
-	        }
-	}
-}	
+
+}
