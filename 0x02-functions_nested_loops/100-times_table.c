b45b674a14c86dbd9cb2f4ff76d4b178efcd8d08
Tpm100 <tokunbo2420@gmail.com>
2022-09-22 08:04:41 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,7 +11,7 @@ void print_times_table(int n)
 	if (n >= 0 && n <= 15)
 	{
 		for (a = 0; a <= n; a++)
-		{	
+		{
 			_putchar(48);
 			for (b = 1; b <=n; b++)
 			{
@@ -30,14 +30,14 @@ void print_times_table(int n)
 					_putchar((op / 10) + 48);
 					_putchar((op % 10) + 48);
 				}
-				else 
+				else
 				{
 					_putchar(((op / 100)  % 10) + 48);
 					_putchar(((op / 10) % 10) + 48);
-					-putchar((op % 10) + 48);
-				}	
+					_putchar((op % 10) + 48);
+				}
 			}
 			_putchar('\n');
 		}
 	}
-}	
+}
