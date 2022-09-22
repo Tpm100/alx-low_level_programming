591ccdb437d5cf364e2b92fa318203d20ff46617
Tpm100 <tokunbo2420@gmail.com>
2022-09-22 04:16:34 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,12 @@
  * Return: 0
  */
 void print_alphabet(void)
-{
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
-	{	
-		_putchar(c);
+{	 
+	char alpha;
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar (alpha);
 	}
-_putchar('\n');
+	_putchar('\n');
 }
+
