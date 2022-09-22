21ec4768b76165d902909c6bea97c756ae9be566
Tpm100 <tokunbo2420@gmail.com>
2022-09-22 09:03:45 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,14 +7,14 @@
 int main(void)
 {
 	long int i, j, k, next;
+
 	j = 1;
 	k = 2;
-
 	for (i = 1; i <= 50; ++i)
 	{
 		if (j != 20365011074)
 		{
-			printf("%ld", j);
+			printf("%ld, ", j);
 		} else
 		{
 			printf("%ld\n", j);
