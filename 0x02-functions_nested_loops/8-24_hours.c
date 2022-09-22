f9e8439a28a9408f7ccfb9f7e6b45d2101de5a76
Tpm100 <tokunbo2420@gmail.com>
2022-09-22 06:23:09 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 /**
  * jack_bauer - function that prints every minute of the day, 24 hour clock
  * h = hour, m = minutes
- */ 10 allows second digit to rotate
+ * 10 allows second digit to rotate
  * for loops breaks before passing 24:00
  * Return: 24 hour clock line by line
  */
