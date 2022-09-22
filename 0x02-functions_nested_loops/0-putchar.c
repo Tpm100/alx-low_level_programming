1fccee70fed1254c077a9f1029b1d0d803efa68c
Tpm100 <tokunbo2420@gmail.com>
2022-09-22 04:09:56 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
 #include "main.h"
-
 /**
  * main - print putchar
+ *
  * Return: always 0
  */
-
 int main(void)
 {
 	_putchar('_');
@@ -16,7 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 	return (0);
-
-}	
+}
