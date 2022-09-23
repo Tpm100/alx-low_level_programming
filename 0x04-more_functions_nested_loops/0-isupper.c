ed5da5f57d69f0729b124ac4c951361ecac9e27c
Tpm100 <tokunbo2420@gmail.com>
2022-09-22 23:16:50 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,15 @@
 #include "main.h"
 #include <ctype.h>
 /**
- * _isupper - function that checks for alphabetical character
+ * _isupper - function that checks for alphabetic character
  *
- * @c: return char type
+ * @c:return char type
  *
  * Return: return 0 on success and 1 on fail
  */
 int _isupper(int c)
 {
-	int i = isupper(int c);
+	int i = isupper(c);
 
 	if (i > 0)
 	{
